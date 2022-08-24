5cdab263d49e68154b5138f1134c602aa010643a
Franco Balich <franco.balich@gmail.com>
2022-08-24 02:06:01 -0300

    Add link

--- src/components/Boton.tsx
+++ src/components/Boton.tsx
@@ -4,16 +4,19 @@ interface propsButton{
     img:any,
     title:string,
     subtitle:string,
-    socialNetwork:string
+    socialNetwork:string,
+    link:string
 }
-function Boton({img,title,subtitle,socialNetwork}:propsButton){
+function Boton({img,title,subtitle,socialNetwork,link}:propsButton){
     return(
         <div className={`buttonContainer ${socialNetwork}`}>
-            <img className="buttonImg" src={img} alt={`Foto de ${socialNetwork}`}/>
-            <div className="textContainer">
-                <p className="buttonTitle">{title}</p>
-                <p className="buttonSubtitle">{subtitle}</p>
-            </div>
+            <a href={link}>
+                <img className="buttonImg" src={img} alt={`Foto de ${socialNetwork}`}/>
+                <div className="textContainer">
+                    <p className="buttonTitle">{title}</p>
+                    <p className="buttonSubtitle">{subtitle}</p>
+                </div>
+            </a>
         </div>
     )
 }
