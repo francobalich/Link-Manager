583f91835a588e49a1376553fe629d31041d5751
Franco Balich <franco.balich@gmail.com>
2022-08-24 01:57:25 -0300

    Add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Presentacion from './components/Presentacion'
+import Boton from './components/Boton'
+import userImg from "./assets/user.png"
+import twitchImg from "./assets/twitchLogo.png"
+import discordImg from "./assets/discordLogo.png"
+import twitterImg from "./assets/TwitterLogo.png"
+import youtubeImg from "./assets/YtLogo.png"
+import shortsImg from "./assets/ShortsLogo.png"
+import tiktokImg from "./assets/TikTokLogo.png"
+import instagramImg from "./assets/IgLogo.png"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +18,52 @@ function App() {
     <div className="App">
       <Presentacion
         userName="Franco Balich"
-        description="💻 Te ayudo a aprender sobre tecnologia."
+        description="💻Te ayudo a aprender sobre tecnologia."
+        img={userImg}
       ></Presentacion>
+
+      <Boton
+        img={twitchImg}
+        title="Twitch🎥"
+        subtitle="FrancoLabs"
+        socialNetwork="twitch"
+      ></Boton>
+      <Boton
+        img={discordImg}
+        title="Discord⚡"
+        subtitle="¡Reuniones todos los Miercoles!"
+        socialNetwork="discord"
+      ></Boton>
+      <Boton
+        img={instagramImg}
+        title="Instagram 📷"
+        subtitle="@francobalich"
+        socialNetwork="instagram"
+      ></Boton>
+      <Boton
+        img={youtubeImg}
+        title="Canal Principal 🎬"
+        subtitle="Franco Balich"
+        socialNetwork="youtube"
+      ></Boton>
+      <Boton
+        img={shortsImg}
+        title="Canal de Shorts 📲"
+        subtitle="Franco Balich Shorts"
+        socialNetwork="shorts"
+      ></Boton>
+      <Boton
+        img={twitterImg}
+        title="Twitter 🐦"
+        subtitle="@francobalich"
+        socialNetwork="twitter"
+      ></Boton>
+      <Boton
+        img={tiktokImg}
+        title="TikTok📱"
+        subtitle="@francobalich"
+        socialNetwork="tiktok"
+      ></Boton>
     </div>
   )
 }
