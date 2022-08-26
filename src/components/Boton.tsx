e4c81ab98f520270bd3febccd72cfb865617c0a0
Franco Balich <franco.balich@gmail.com>
2022-08-26 01:51:59 -0300

    Add import dynamic img

--- src/components/Boton.tsx
+++ src/components/Boton.tsx
@@ -1,24 +1,30 @@
 import React from "react";
+import {useState, useEffect} from "react"
 import "../styles/Boton.css"
+
 interface propsButton{
-    img:any,
+    img:string,
     title:string,
     subtitle:string,
-    socialNetwork:string,
+    style:string,
     link:string
 }
-function Boton({img,title,subtitle,socialNetwork,link}:propsButton){
-    return(
-        <div className={`buttonContainer no-seleccionable ${socialNetwork}`}>
-            <a href={link}>
-                <img className="buttonImg" src={img} alt={`Foto de ${socialNetwork}`}/>
-                <div className="textContainer">
-                    <p className="buttonTitle">{title}</p>
-                    <p className="buttonSubtitle">{subtitle}</p>
-                </div>
-            </a>
+function Boton({img,title,subtitle,style,link}:propsButton){
+    const [image, setImage] = useState("")
+    function importImage(imageName:string){
+        import(`../assets/${imageName}.png`)
+        .then((image)=>setImage(image.default));
+    }
+    importImage(img)
+    return(<div className={`buttonContainer no-seleccionable ${style}`}>
+    <a href={link}>
+        <img className="buttonImg" src={image} alt={`Foto de ${style}`}/>
+        <div className="textContainer">
+            <p className="buttonTitle">{title}</p>
+            <p className="buttonSubtitle">{subtitle}</p>
         </div>
-    )
+    </a>
+</div>)
 }
 
 export default Boton;
